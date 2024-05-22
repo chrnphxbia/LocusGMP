d054cf40526f41b1f5494904626d72397a86395f
lihviaa <liviaalabarse@gmail.com>
2024-05-22 12:47:39 -0300

    mudanças nas classes de entidade

--- src/main/Entity/Anfitriao.java
+++ src/main/Entity/Anfitriao.java
@@ -11,16 +11,16 @@ public class Anfitriao {
 
     public Anfitriao(String nome, String contaBancaria){
         this.nome = nome;
-        this.contaBancaria = contaBancaria.
-        listaImoveis = new LinkedList();
-        listaReservas = new LinkedList();
+        this.contaBancaria = contaBancaria;
+        this.listaImoveis = new LinkedList<>();
+        this.listaReservas = new LinkedList<>();
     }
 
     public String getNome() {return nome;}
     public void setNome(String nome) {this.nome = nome;}
 
     public List<Imovel> getListaImoveis() {return listaImoveis;}
-    public void setListaImoveis(List<Imovel> listaImovel) {this.listaImovel = listaImovel;}
+    public void setListaImoveis(List<Imovel> listaImovel) {this.listaImoveis = listaImovel;}
 
     public String getContaBancaria() {return contaBancaria;}
     public void setContaBancaria(String contaBancaria) {this.contaBancaria = contaBancaria;}
@@ -29,17 +29,17 @@ public class Anfitriao {
     public void addReserva(Reserva reserva) {listaReservas.add(reserva);}
     public void removeReserva(Reserva reserva) {listaReservas.remove(reserva);}
 
-    public void addImovel(TipoImovel tipoImovel, String aparelho, double orcamento) {
-        Imovel Imovel = new Imovel(tipoImovel, aparelho, orcamento);
-        listaImovels.add(Imovel);
+    public void addImovel(String id, String endereco, List<String> datasDisponiveis, String tipoImovel, int nHospedes, int nQuartos, int nBanheiros, float avaliacao, float preco) {
+        Imovel imovel = new Imovel(id, endereco, datasDisponiveis, tipoImovel, nHospedes, nQuartos, nBanheiros, avaliacao, preco);
+        listaImoveis.add(imovel);
     }
 
-    public void removeImovel(Imovel Imovel) {listaImovels.remove(Imovel);}
+    public void removeImovel(Imovel Imovel) {listaImoveis.remove(Imovel);}
 
-    public Imovel getImovel(TipoImovel Imovel, String aparelho) {
-        for(int i = 0; i < listaImovels.size(); ++i) {
-            if(listaImovels.get(i).getTipoImovel() == Imovel && listaImovels.get(i).getAparelho().equals(aparelho)) {
-                return listaImovels.get(i);
+    public Imovel getImovel(String id) {
+        for(int i = 0; i < listaImoveis.size(); ++i) {
+            if(listaImoveis.get(i).getId() == id) {
+                return listaImoveis.get(i);
             }
         }
 
--- src/main/Entity/Imovel.java
+++ src/main/Entity/Imovel.java
@@ -3,16 +3,18 @@ package Entity;
 import java.util.List;
 
 public class Imovel {
+    private String id;
     private String endereco;
     private List<String> datasDisponiveis;
     private String tipoImovel;
     private int nHospedes;
     private int nQuartos;
     private int nBanheiros;
-    private double avaliacao;
-    private double preco;
+    private float avaliacao;
+    private float preco;
 
-    public Imovel(String endereco, List<String> datasDisponiveis, String tipoImovel, int nHospedes, int nQuartos, int nBanheiros, double avaliacao, double preco) {
+    public Imovel(String id, String endereco, List<String> datasDisponiveis, String tipoImovel, int nHospedes, int nQuartos, int nBanheiros, float avaliacao, float preco) {
+        this.id = id;
         this.endereco = endereco;
         this.datasDisponiveis = datasDisponiveis;
         this.tipoImovel = tipoImovel;
@@ -23,9 +25,11 @@ public class Imovel {
         this.preco = preco;
     }
 
-
-    public getEndereco() {return endereco;}
-    public setEndereco(String endereco) {this.endereco = endereco;}
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+    
+    public String getEndereco() {return endereco;}
+    public void setEndereco(String endereco) {this.endereco = endereco;}
 
     public List<String> getDatasDisponiveis() {return datasDisponiveis;}
     public void setDatasDisponiveis(List<String> datasDisponiveis) {this.datasDisponiveis = datasDisponiveis;}
@@ -42,11 +46,11 @@ public class Imovel {
     public int getNBanheiros() {return nBanheiros;}
     public void setNBanheiros(int nBanheiros) {this.nBanheiros = nBanheiros;}
 
-    public double getAvaliacao() {return avaliacao;}
-    public void setAvaliacao(double avaliacao) {this.avaliacao = avaliacao;}
+    public float getAvaliacao() {return avaliacao;}
+    public void setAvaliacao(float avaliacao) {this.avaliacao = avaliacao;}
 
-    public double getPreco() {return preco;}
-    public void setPreco(double preco) {this.preco = preco;}
+    public float getPreco() {return preco;}
+    public void setPreco(float preco) {this.preco = preco;}
 
     @Override
     public String toString() {
@@ -55,14 +59,15 @@ public class Imovel {
 
     public Imovel fromString(String imovelStr) {
         String[] partes = imovelStr.split(",");
-        String endereco = partes[0];
-        List<String> datasDisponiveis = List.of(partes[1].split(";"));
-        String tipoImovel = partes[2];
-        int nHospedes = Integer.parseInt(partes[3]);
-        int nQuartos = Integer.parseInt(partes[4]);
-        int nBanheiros = Integer.parseInt(partes[5]);
-        double avaliacao = Double.parseDouble(partes[6]);
-        double preco = Double.parseDouble(partes[7]);
-        return new Imovel(endereco, datasDisponiveis, tipoImovel, nHospedes, nQuartos, nBanheiros, avaliacao, preco);
+        String id = partes[0];
+        String endereco = partes[1];
+        List<String> datasDisponiveis = List.of(partes[2].split(";"));
+        String tipoImovel = partes[3];
+        int nHospedes = Integer.parseInt(partes[4]);
+        int nQuartos = Integer.parseInt(partes[5]);
+        int nBanheiros = Integer.parseInt(partes[6]);
+        float avaliacao = Float.parseFloat(partes[7]);
+        float preco = Float.parseFloat(partes[8]);
+        return new Imovel(id, endereco, datasDisponiveis, tipoImovel, nHospedes, nQuartos, nBanheiros, avaliacao, preco);
     }
 }
